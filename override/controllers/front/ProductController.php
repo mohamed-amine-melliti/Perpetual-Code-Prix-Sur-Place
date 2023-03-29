3874f71d7f6d43798d4df5d9456c473b54d51637
Mohamed Amine Melliti <mohamedamine.melliti@esprit.tn>
2023-03-29 05:21:37 +0100

    2

--- override/controllers/front/ProductController.php
+++ override/controllers/front/ProductController.php
@@ -1,1 +1,15 @@
 <?php
+class ProductController extends ProductControllerCore
+{
+    public function initContent()
+    {
+        parent::initContent();
+
+        $id_product = (int) Tools::getValue('id_product');
+        $product = new Product($id_product);
+
+        $this->context->smarty->assign(array(
+            'prixsurplace_value' => $product->prix_sur_place,
+        ));
+    }
+}
--- sql/install.php
+++ sql/install.php
@@ -35,3 +35,38 @@ foreach ($sql as $query) {
         return false;
     }
 }
+
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class PrixSurPlaceInstall
+{
+    public function __construct()
+    {
+        $this->createTable();
+    }
+
+    public function createTable()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS `" . _DB_PREFIX_ . "product_prix_sur_place` (
+            `id_product` INT(11) NOT NULL,
+            `prix_sur_place` DECIMAL(20,6) NOT NULL DEFAULT '0.000000',
+            PRIMARY KEY (`id_product`)
+        ) ENGINE=" . _MYSQL_ENGINE_ . " DEFAULT CHARSET=utf8;";
+
+        return Db::getInstance()->execute($sql);
+    }
+
+    public function uninstall()
+    {
+        $sql = "DROP TABLE IF EXISTS `" . _DB_PREFIX_ . "product_prix_sur_place`";
+        return Db::getInstance()->execute($sql);
+    }
+}
+
+$install = new PrixSurPlaceInstall();
+
+
+
